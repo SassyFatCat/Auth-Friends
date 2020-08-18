e73fc0319aee6323bd89eee5963e9026e71eabcb
Ryan Lee <therealsassyfatcat@outlook.com>
2020-08-18 15:48:29 -0500

    saving

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Link, Switch, Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import PrivateRoute from './Components/PrivateRoute';
+import Login from './Components/Login';
+import Dashboard from './Components/Dashboard';
 
 function App() {
+const [form, setForm] = useState({
+  username: 'Lambda School',
+  password: 'i<3Lambd4'
+});
+const [loading, setLoading] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <section>
+      <header>
+          <h1>Auth-Friends App</h1>
+            <nav className='nav'>
+              <Link className='link' to='/login'>Login</Link>
+              <Link className='link' to='dashboard'>Dashboard</Link>
+            </nav>
       </header>
-    </div>
+      
+      <Switch>
+        <Route exact path='/'>
+          <h2>Welcome to Auth-Friends App</h2>
+        </Route>
+
+        <Route path='/login' component={Login} />
+
+        <PrivateRoute path='/dashboard' component={Dashboard} />
+      </Switch>
+
+    </section>
   );
 }
 
